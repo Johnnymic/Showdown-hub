ecfa298a6d4044eee444885b1b5021d4116461b5
Johnnymic <johnny25580@gmail.com>
2024-08-03 21:40:53 +0100

    commit day16

---
+++ Models/Dtos/AccountResponse.cs
@@ -0,0 +1,15 @@
+namespace Showdown_hub.Models.Dtos
+{
+    public class AccountResponse
+    {
+        public string AccountName { get; set; }
+
+
+     public string BankName { get; set; }
+
+      public string BankType { get; set;}
+
+    public string AccountNumber { get; set; }
+        
+    }
+}
